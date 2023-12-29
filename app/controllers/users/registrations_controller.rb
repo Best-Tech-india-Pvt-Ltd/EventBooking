9d6a2c10ac6dee8a7b73c82b3e2b13c34596d551
Bhavuk Sidhu <bhavuksidhu@gmail.com>
2023-12-29 12:41:12 +0530

    All files

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
+  respond_to :json
+  skip_before_action :verify_authenticity_token
+
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+
+    resource.role = "customer"
+    resource.save
+    
+    yield resource if block_given?
+    if resource.persisted?
+      sign_in(resource_name, resource)
+      render json: {
+       status: { code: 200, message: "Signed Up sucessfully", data: resource, authorization: "Bearer #{request.env['warden-jwt_auth.token']}"}
+     }
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def create_event_organizer
+    build_resource(sign_up_params)
+    resource.role = "event_organizer"
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      sign_in(resource_name, resource)
+      token = Warden::JWTAuth::UserEncoder.new.call(resource, :user, nil).first
+      render json: {
+       status: { code: 200, message: "Signed Up sucessfully", data: resource, authorization: "Bearer #{token}"}
+     }
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,60 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2023_12_28_104248) do
+  create_table "bookings", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.integer "ticket_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_bookings_on_event_id"
+    t.index ["ticket_id"], name: "index_bookings_on_ticket_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "event_name"
+    t.date "event_date"
+    t.string "event_venue"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.string "name"
+    t.decimal "price"
+    t.integer "availability"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_tickets_on_event_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "jti", null: false
+    t.integer "role"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "bookings", "events"
+  add_foreign_key "bookings", "tickets"
+  add_foreign_key "tickets", "events"
+end
